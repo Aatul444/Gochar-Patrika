365ac793cc9a67846c502d4d869640c909e3888d
apple <apple@apples-MacBook-Pro.local>
2022-12-24 18:19:53 +0530

    to rishant

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAPCzh_4CMd2K6urD6V0wGNInJ3_Py3Q_E",
+  authDomain: "appusers-725e2.firebaseapp.com",
+  databaseURL: "https://appusers-725e2-default-rtdb.firebaseio.com",
+  projectId: "appusers-725e2",
+  storageBucket: "appusers-725e2.appspot.com",
+  messagingSenderId: "420574719552",
+  appId: "1:420574719552:web:cd820e0ccb7a33b0a439a5"
+  }
 };
